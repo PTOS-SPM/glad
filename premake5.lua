d0569fbf7c51498b0ebe94a122dccc8a63bac4c5
SZB3748 <jakengoodyear@gmail.com>
2023-12-06 15:08:05 -0500

    Edit premake file
    Forgot to remove unnecessary files :P

--- premake5.lua
+++ premake5.lua
@@ -11,31 +11,14 @@ project "GLAD"
         "src/glad.c"
     }
 
+    includedirs {
+        "include"
+    }
+
     filter "system:windows"
         buildoptions { "/MT" } 
         systemversion "latest"
         staticruntime "On"
-
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_module.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.h",
-            "src/win32_time.c",
-            "src/win32_thread.h",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
-
-        defines {
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
 
     filter "configurations:Debug"
         runtime "Debug"
